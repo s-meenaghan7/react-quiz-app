b86683a3b3a78b2ee1d2bdf2eb63005413e2f18a
Sean Meenaghan <seanmeenaghan@gmail.com>
2022-08-17 18:51:52 -0700

    implemented score keeping

--- src/App.js
+++ src/App.js
@@ -1,31 +1,64 @@
 import { useState } from 'react';
 import './App.css';
 
+/*
+Issues/todos:
+  Making no selection and pressing next or prev button causes errors because of null 'value' of radio button
+  May be able to (And should do this if I can) remove the setScoreArr function declaration entirely, to prevent the warning.
+*/
+
 function App() {
-  let [q_index, setQIndex] = useState(0);
+  let [i, setIndex] = useState(0); // i accesses both the questions and scoreArr data structures
+  const [scoreArr, setScoreArr] = useState([]);
+
+  const getValue = () => (document.querySelector('input[name = "answers"]:checked').value === 'true'); 
+
+  const prevButtonHandler = () => {
+    setIndex((i > 0) ? i - 1 : i);
+    scoreArr[i] = getValue() ? 1 : 0;
+  };
+
+  const nextButtonHandler = () => {
+    setIndex((i < questions.length - 1) ? i + 1 : i);
+    scoreArr[i] = getValue() ? 1 : 0;
+  };
 
   return (
     <div className="App">
       <div className='question-section'>
-        <h4>Question {q_index + 1}/{questions.length}</h4>
-        <h2>{questions[q_index].question}</h2>
+        <h4>Question {i + 1}/{questions.length}</h4>
+        <h2>{questions[i].question}</h2>
       </div>
 
-      <div className='answer-section'>
-        <label> <input type='radio' name='answers' value='option1' /> {questions[q_index].options[0].answer} </label>
-        <label> <input type='radio' name='answers' value='option2' /> {questions[q_index].options[1].answer} </label>
-        <label> <input type='radio' name='answers' value='option3' /> {questions[q_index].options[2].answer} </label>
-        <label> <input type='radio' name='answers' value='option4' /> {questions[q_index].options[3].answer} </label>
+      <div className='answer-section'> {/* TODO: render answers dynamically with map (or foreach loop?)*/}
+        <label>
+          <input type='radio' name='answers' value={questions[i].options[0].isCorrect} /> {questions[i].options[0].answer}
+        </label>
+        <label>
+          <input type='radio' name='answers' value={questions[i].options[1].isCorrect} /> {questions[i].options[1].answer}
+        </label>
+        <label>
+          <input type='radio' name='answers' value={questions[i].options[2].isCorrect} /> {questions[i].options[2].answer}
+        </label>
+        <label>
+          <input type='radio' name='answers' value={questions[i].options[3].isCorrect} /> {questions[i].options[3].answer}
+        </label>
       </div>
 
       <div className='controls'>
-        <button onClick={() => (q_index > 0) ? setQIndex(q_index - 1) : q_index}>
+        <button onClick={() => prevButtonHandler() }>
           Previous
         </button>
 
-        <button onClick={() => (q_index < questions.length - 1) ? setQIndex(q_index + 1) : q_index}>
-          Submit
+        <button onClick={ () => nextButtonHandler() }>
+          {(i === questions.length - 1) ? 'Submit' : 'Next'}
         </button>
+
+        {/* test button, delete later */}
+        <button onClick={() => console.log(scoreArr.reduce((total, curr) => { return total + curr; }, 0))}>
+          Get Score
+        </button>
+
       </div>
     </div>
   );
