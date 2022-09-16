866b7400c4fcf982862d0e3c27ae4f8aaf1c25bb
Sean Meenaghan <seanmeenaghan@gmail.com>
2022-09-15 17:59:45 -0700

    score percentage displayed in quiz result to user

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 export default function Result({points, total}) {
+
+    const percentage = (( points / total ) * 100).toFixed(0) + '%';
+
     return (
         <div className='App'>
             <h1>Quiz Complete!</h1>
-            <h2>Final score: {points} out of {total}</h2>
+            <h2>Final score: {points} out of {total} ({ percentage })</h2>
         </div>
     );
 }
