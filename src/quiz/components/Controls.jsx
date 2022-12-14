72190cfd47e79ef58b3e735b61f4544ff4718f28
Sean Meenaghan <seanmeenaghan@gmail.com>
2022-12-13 20:56:03 -0800

    Previous and Next Question buttons now disable accordingly.

--- src/quiz/components/Controls.jsx
+++ src/quiz/components/Controls.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../styles/Controls.css';
 
 export default function Controls({ index, setIndex, scoreArr, selectedAnswers, selectedAnswer, setSelectedAnswer, setQuizSubmitted }) {
+
+  useEffect(() => { // Determine if Next and Previous question buttons are disabled/enabled.
+    const prevBtn = document.getElementById('prev');
+    const nextBtn = document.getElementById('next');
+
+    if (index === 0) {
+      prevBtn.setAttribute('disabled', true);
+    } else {
+      prevBtn.removeAttribute('disabled');
+    }
+
+    if (index === (selectedAnswers.length - 1)) {
+      nextBtn.setAttribute('disabled', true);
+    } else {
+      nextBtn.removeAttribute('disabled');
+    }
+  }, [index, selectedAnswers.length]);
+
   // gives 1 point if the selected answer for this question is true, 0 if not.
   const getValue = () => (document.querySelector('input[name = "answers"]:checked').value === 'true') ? 1 : 0;
 
@@ -46,16 +64,16 @@ export default function Controls({ index, setIndex, scoreArr, selectedAnswers, s
 
   return (
     <div className='controls-container'>
-      <button type='button' className='controls-button' onClick={() => prevButtonHandler()}>
-        Previous
+      <button id='prev' type='button' className='controls-button' onClick={() => prevButtonHandler()}>
+        Previous Question
       </button>
 
       <button type='button' className='controls-button' id='submit' onClick={() => submitQuiz(scoreArr)} hidden>
         Submit
       </button>
 
-      <button type='button' className='controls-button' onClick={() => nextButtonHandler()}>
-        Next
+      <button id='next' type='button' className='controls-button' onClick={() => nextButtonHandler()}>
+        Next Question
       </button>
     </div>
   );
