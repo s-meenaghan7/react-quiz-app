24314e47db902731e3e7e6ba36208b35de05507e
Sean Meenaghan <seanmeenaghan@gmail.com>
2022-12-13 20:23:04 -0800

    Changed CSS and JSX to implement better looking answers: the radio buttons line up regardless of the answer text, and the answer text is larger.

--- src/quiz/components/Answer.jsx
+++ src/quiz/components/Answer.jsx
@@ -3,15 +3,18 @@ import '../styles/Answer.css';
 
 export default function Answer({ answer, answerChanged, selectedAnswer }) {
   return (
-    <label className='answer'>
-      <input
-        type='radio'
-        name='answers'
-        className='radio'
-        value={answer.isCorrect}
-        onChange={() => answerChanged(answer.id)}
-        checked={selectedAnswer === answer.id}
-      /> {answer.answer}
-    </label>
+    <div className='answer-container'>
+      <label className='answer-button'>
+        <input
+          type='radio'
+          name='answers'
+          className='radio'
+          value={answer.isCorrect}
+          onChange={() => answerChanged(answer.id)}
+          checked={selectedAnswer === answer.id}
+        />
+      </label>
+      <span className='answer'> {answer.answer} </span>
+    </div>
   );
 }
